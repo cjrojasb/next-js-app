e551c6c31f2ffc355289ba4d67b164ec59f13400
Carlos Rojas Burgos <cjrojasb@falabella.cl>
2023-08-07 15:18:28 -0400

    feat: was add next/image to comments

--- app/(shared-posts-layout)/posts/[id]/comments/page.tsx
+++ app/(shared-posts-layout)/posts/[id]/comments/page.tsx
@@ -1,7 +1,8 @@
 import { Comment } from "@/interfaces/Comment";
+import Image from "next/image";
 
 const fetchCommentsByPostId = async (id: string) => {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   // throw new Error("Error al cargar los comentarios")
   return fetch(
@@ -16,7 +17,18 @@ async function CommentsPage({ params }: any) {
     <article className="my-5 bg-gray-400 p-4 bg-black">
       {comments.map(({ name, email, body, id }) => (
         <div key={id} className="flex flex-col mb-3">
-          <h3 className="capitalize text-lg font-bold text-blue-400">{name}</h3>
+          <div className="flex flex-row items-center gap-3">
+            <h3 className="capitalize text-lg font-bold text-blue-400">
+              {name}
+            </h3>
+            <Image
+              alt={name}
+              src={`https://avatars.dicebear.com/api/pixel-art-neutral/${email}.svg`}
+              width={50}
+              height={50}
+              className="rounded-full"
+            />
+          </div>
           <div className="flex flex-col text-white">
             <p className="text-base">{body}</p>
             <p className="text-sm">{email}</p>
